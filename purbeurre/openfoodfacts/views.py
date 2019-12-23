d87f74193f6d88c2cff7b5893cbd41cc976f73e0
Julien Ringenbach <ringenbach.julien@gmail.com>
2019-12-23 14:54:11 +0100

    Templates built for the index page from a boostrap template

--- purbeurre/openfoodfacts/views.py
+++ purbeurre/openfoodfacts/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Page d'introduction")
+    return render(request, "openfoodfacts/index.html")
+
+def connexion(request):
+    return render(request, "openfoodfacts/connexion.html")
